dd51dd2f629538fa9235c75d67de7db5c79dd3d6
carter <cpsiff@gmail.com>
2019-09-24 12:44:18 -0500

    Initial commit

---
+++ src/bashShell/MyScanner.java
@@ -0,0 +1,90 @@
+package bashShell;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MyScanner {
+
+    private int nextToken;
+
+    private ArrayList<Byte> tokens = null;
+
+    public MyScanner(String sentence){
+        Scanner sent = new Scanner(sentence);
+        tokens = new ArrayList<Byte>();
+
+        while(sent.hasNext()) {
+            String temp = sent.next();
+            switch (temp) {
+                case "cat":
+                case "ls":
+                case "pwd":
+                case "touch":
+                case "cp":
+                case "mv":
+                case "rm":
+                case "chmod":
+                case "man":
+                case "ps":
+                case "bg":
+                    tokens.add(Token.FName);
+                    System.out.println(Token.FName);
+                    break;
+                case "=":
+                    tokens.add(Token.ASSIGN);
+                    break;
+                case "if":
+                    tokens.add(Token.IF);
+                    break;
+                case "then":
+                    tokens.add(Token.THEN);
+                    break;
+                case "else":
+                    tokens.add(Token.ELSE);
+                    break;
+                case "fi":
+                    tokens.add(Token.FI);
+                    break;
+                case "for":
+                    tokens.add(Token.FOR);
+                    break;
+                case "in":
+                    tokens.add(Token.IN);
+                    break;
+                case "do":
+                    tokens.add(Token.DO);
+                    break;
+                case "od":
+                    tokens.add(Token.OD);
+                    break;
+                case "eol":
+                    tokens.add(Token.EOL);
+                    break;
+                default:
+                    if (temp.matches("[a-zA-Z][a-zA-Z0-9_]*")){
+                        tokens.add(Token.VAR);
+                    }
+                    else if(temp.matches("-?[a-zA-Z0-9]*|[0-9]")){
+                        tokens.add(Token.LIT);
+                    }
+                    else{
+                        throw new RuntimeException("Unexpected token: " + temp);
+                    }
+            }
+        }
+        System.out.println(tokens);
+        nextToken = 0;
+    }
+
+    public Byte nextToken() {
+        if (nextToken < tokens.size()){
+            nextToken++;
+            return(tokens.get(nextToken - 1));
+        }
+        else return Token.EOT;
+    }
+
+    public static void main(String [] args) {
+        Parser ts = new Parser("abc = cat eol");
+    }
+}
---
+++ src/bashShell/Parser.java
@@ -0,0 +1,153 @@
+package bashShell;
+
+import java.util.*;
+
+public class Parser {
+    private Byte currentToken = null;
+    private MyScanner myScanner = null;
+
+    //------------- Utility Methods -------------
+
+    /**
+     * Accept a specified token if it matches the
+     * current Token.  Acceptance entails setting
+     * currentToken to the next token in the input
+     * stream.
+     *
+     * @param expectedKind The expected type of token.
+     */
+    private void accept(byte expectedKind) {
+        if (currentToken == expectedKind)
+            currentToken = myScanner.nextToken();
+        else
+            writeError("Expected:  " + Token.kindString(expectedKind) +
+                    "Found :" + Token.kindString(currentToken));
+    }
+
+    /**
+     * Accept the current token by setting currentToken
+     * to the next token in the input stream.
+     */
+    private void acceptIt() {
+        currentToken = myScanner.nextToken();
+    }
+
+    private void writeError(String s) {
+        System.out.println(s);
+    }
+
+    //---------------- Public Methods ----------------
+
+    public Parser(String sentence){
+        myScanner = new MyScanner(sentence);
+        currentToken = myScanner.nextToken();
+        parseScript();
+    }
+
+    //---------------- Parsing Methods ---------------
+    private void parseScript() {
+        while (currentToken == Token.FName
+                || currentToken == Token.VAR
+                || currentToken == Token.IF
+                || currentToken == Token.FOR)
+            parseCommand();
+    }
+
+    private void parseCommand() {
+        switch (currentToken) {
+            case Token.FName: {
+                parseFileName();
+                // while in the argument, which can go to FName, LIT, or VAR
+                while (currentToken == Token.FName
+                        || currentToken == Token.LIT
+                        || currentToken == Token.VAR)
+                    parseArgument();
+                accept(Token.EOL);
+                break;
+            }
+            case Token.VAR: {
+                parseVariable();
+                accept(Token.ASSIGN);
+                while (currentToken == Token.FName
+                        || currentToken == Token.LIT
+                        || currentToken == Token.VAR)
+                    parseArgument();
+                accept(Token.EOL);
+                break;
+            }
+            case Token.IF: {
+                acceptIt();
+                parseFileName();
+                while (currentToken == Token.FName
+                        || currentToken == Token.LIT
+                        || currentToken == Token.VAR)
+                    parseArgument();
+                accept(Token.THEN);
+                accept(Token.EOL);
+                while(currentToken == Token.FName
+                        || currentToken == Token.VAR
+                        || currentToken == Token.IF
+                        || currentToken == Token.FOR)
+                    parseCommand();
+                accept(Token.ELSE);
+                accept(Token.EOL);
+                while(currentToken == Token.FName
+                        || currentToken == Token.VAR
+                        || currentToken == Token.IF
+                        || currentToken == Token.FOR)
+                    parseCommand();
+                accept(Token.FI);
+                accept(Token.EOL);
+                break;
+            }
+            case Token.FOR: {
+                acceptIt();
+                parseVariable();
+                accept(Token.IN);
+                while (currentToken == Token.FName
+                        || currentToken == Token.LIT
+                        || currentToken == Token.VAR)
+                    parseArgument();
+                accept(Token.EOL);
+                accept(Token.DO);
+                while(currentToken == Token.FName
+                        || currentToken == Token.VAR
+                        || currentToken == Token.IF
+                        || currentToken == Token.FOR)
+                    parseCommand();
+                accept(Token.OD);
+                accept(Token.EOL);
+                break;
+            }
+        }
+    }
+
+    private void parseArgument() {
+        switch(currentToken) {
+            case Token.FName: {
+                parseFileName();
+                break;
+            }
+            case Token.LIT: {
+                parseLiteral();
+                break;
+            }
+            case Token.VAR: {
+                parseVariable();
+                break;
+            }
+        }
+    }
+
+    private void parseFileName() {
+        acceptIt();
+    }
+
+    private void parseLiteral() {
+        acceptIt();
+    }
+
+    private void parseVariable() {
+        acceptIt();
+    }
+}
